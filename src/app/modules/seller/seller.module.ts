9da024f4f6a63b8603a981e2bce76eace5952f9b
Miray Sönmez <113294105+chiturca@users.noreply.github.com>
2024-02-26 17:22:46 +0300

    Feat: add getAll to sellers

--- src/app/modules/seller/seller.module.ts
+++ src/app/modules/seller/seller.module.ts
@@ -1,12 +1,40 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
+import { SellersService } from './services/sellers.service';
 
 import { SellerRoutingModule } from './seller-routing.module';
 import { SellerComponent } from './component/seller.component';
 import { RoutesContainerModule } from '../shared/routes-container/routes-container.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
   declarations: [SellerComponent],
-  imports: [CommonModule, SellerRoutingModule, RoutesContainerModule],
+  imports: [
+    CommonModule,
+    SellerRoutingModule,
+    RoutesContainerModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatTableModule,
+    MatSortModule,
+    MatIconModule,
+    MatInputModule,
+    MatButtonModule,
+    MatTooltipModule,
+    MatDialogModule,
+    MatPaginatorModule,
+  ],
+  providers: [SellersService, DatePipe],
 })
 export class SellerModule {}
