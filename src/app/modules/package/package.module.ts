319db57f3e7a6e22a4ba4850422c8d602751e815
Miray Sönmez <113294105+chiturca@users.noreply.github.com>
2024-02-28 19:03:13 +0300

    Style: add packages ui

--- src/app/modules/package/package.module.ts
+++ src/app/modules/package/package.module.ts
@@ -4,9 +4,37 @@ import { CommonModule } from '@angular/common';
 import { PackageRoutingModule } from './package-routing.module';
 import { PackageComponent } from './component/package.component';
 import { RoutesContainerModule } from '../shared/routes-container/routes-container.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @NgModule({
   declarations: [PackageComponent],
-  imports: [CommonModule, PackageRoutingModule, RoutesContainerModule],
+  imports: [
+    CommonModule,
+    PackageRoutingModule,
+    RoutesContainerModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatTableModule,
+    MatSortModule,
+    MatIconModule,
+    MatInputModule,
+    MatButtonModule,
+    MatTooltipModule,
+    MatDialogModule,
+    MatPaginatorModule,
+    MatExpansionModule,
+  ],
 })
 export class PackageModule {}
