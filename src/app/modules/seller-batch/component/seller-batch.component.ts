681ffaee9d62ad972cab5ab2f357884668399df1
Miray Sönmez <113294105+chiturca@users.noreply.github.com>
2024-02-29 23:09:30 +0300

    Style: add sellerBatch ui

--- src/app/modules/seller-batch/component/seller-batch.component.ts
+++ src/app/modules/seller-batch/component/seller-batch.component.ts
@@ -1,10 +1,116 @@
-import { Component } from '@angular/core';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
+import { DatePipe } from '@angular/common';
+import {
+  AfterViewInit,
+  ChangeDetectorRef,
+  Component,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { MatAutocomplete } from '@angular/material/autocomplete';
+import { MatDialog } from '@angular/material/dialog';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort, Sort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { debounceTime, distinctUntilChanged, map } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
+import { UserService } from '../../user/services/user.service';
+import { GetUserByRefreshTokenResponseDtoModel } from '../../user/models/response/get-user-by-refresh-token-response-dto-model';
+import { EntityStatuses } from '../../customer/models/enums/entity-statuses';
 
 @Component({
   selector: 'app-seller-batch',
   templateUrl: './seller-batch.component.html',
-  styleUrls: ['./seller-batch.component.css']
+  styleUrls: ['./seller-batch.component.css'],
 })
-export class SellerBatchComponent {
-
+export class SellerBatchComponent implements OnInit, AfterViewInit {
+  isLoaded: boolean = false;
+  getUserFromAuthByDtoModel: GetUserByRefreshTokenResponseDtoModel;
+  searchInputControl: FormControl = new FormControl('');
+  // dataSource!: MatTableDataSource<>;
+  dataSource: any;
+  @ViewChild(MatSort) sort!: MatSort;
+  @ViewChild(MatAutocomplete) auto!: MatAutocomplete;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  constructor(
+    private userService: UserService,
+    private toastrService: ToastrService,
+    private changeDetectorRef: ChangeDetectorRef,
+    private _liveAnnouncer: LiveAnnouncer,
+    private dialog: MatDialog,
+    private datePipe: DatePipe
+  ) {
+    // this.dataSource = new MatTableDataSource<>();
+    this.getUserFromAuthByDtoModel =
+      {} as GetUserByRefreshTokenResponseDtoModel;
+  }
+  ngOnInit(): void {
+    this.setupFilter();
+  }
+  ngAfterViewInit(): void {
+    // this.dataSource.paginator = this.paginator;
+    // this.dataSource.sort = this.sort;
+    this.changeDetectorRef.detectChanges();
+  }
+  setupFilter(): void {
+    this.searchInputControl.valueChanges
+      .pipe(
+        debounceTime(150),
+        distinctUntilChanged(),
+        map((value) => (typeof value === 'string' ? value : ''))
+      )
+      .subscribe((value) => {
+        this.applyFilter(value);
+      });
+  }
+  applyFilter(filterValue: string) {
+    // this.dataSource.filter = filterValue.trim().toLowerCase();
+    // if (this.dataSource.paginator) {
+    //   this.dataSource.paginator.firstPage();
+    // }
+  }
+  announceSortChange(sortState: Sort) {
+    if (sortState.direction) {
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+    } else {
+      this._liveAnnouncer.announce('Sorting cleared');
+    }
+  }
+  getUserFromAuthByDto() {
+    this.userService.getUserFromAuthByDto().subscribe((response) => {
+      this.getUserFromAuthByDtoModel = response.data;
+      this.isLoaded = false;
+    });
+  }
+  mapEntityStatus(type: EntityStatuses): string {
+    switch (type) {
+      case EntityStatuses.active:
+        return 'Aktif Batch';
+      case EntityStatuses.inactive:
+        return 'Pasif Batch';
+      default:
+        return 'Bilinmeyen Paket Tipi';
+    }
+  }
+  getColor(type: EntityStatuses): string {
+    switch (type) {
+      case EntityStatuses.active:
+        return 'rgb(34 197 94)';
+      case EntityStatuses.inactive:
+        return 'red';
+      default:
+        return 'Bilinmeyen Batch Tipi';
+    }
+  }
+  getBg(type: EntityStatuses): string {
+    switch (type) {
+      case EntityStatuses.active:
+        return 'rgba(34, 197, 94, 0.2)';
+      case EntityStatuses.inactive:
+        return 'rgba(255, 0, 0, 0.2)';
+      default:
+        return 'Bilinmeyen Batch Tipi';
+    }
+  }
 }
